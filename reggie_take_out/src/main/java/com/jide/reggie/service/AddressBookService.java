059b6f5b3029073f90ad01a75ef5bc1de311161a
JellyPiggy <101648443+JellyPiggy@users.noreply.github.com>
2022-07-10 18:41:46 +0800

    Delete reggie_take_out directory

--- reggie_take_out/src/main/java/com/jide/reggie/service/AddressBookService.java
+++
@@ -1,8 +0,0 @@
-package com.jide.reggie.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.jide.reggie.entity.AddressBook;
-
-public interface AddressBookService extends IService<AddressBook> {
-
-}
